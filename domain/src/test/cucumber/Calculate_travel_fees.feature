9c7b6bc7b099757cdc358dcf76f754a0a7d72636
Dan <magier.dan.pro@gmail.com>
2018-04-25 15:25:43 +0200

    Adding cucumber scenarios and the signature of the java glue code

--- domain/src/test/cucumber/Calculate_travel_fees.feature
+++ domain/src/test/cucumber/Calculate_travel_fees.feature
@@ -1,2 +1,25 @@
 Feature: as a travel agency, I want to calculate travel fees depending on the departure and destination trip
   The travel price is computed using the travel fees and the agency fees added together.
+
+
+  Scenario Outline: Determine the fees for a supported destination
+    Given the customer want to travel to <destination>
+    And the travel fees are <travelFees>€
+    And the agency fees are <agencyFees>€
+    When the system calculate the trip price
+    Then the trip price is <travelPrice>€
+
+    Examples:
+      | destination | travelFees | agencyFees | travelPrice |
+      | "Paris"     | 800        | 50         | 850         |
+      | "Lille"     | 0          | 0          | 0           |
+      | "New-York"  | 1000       | 100        | 1100        |
+      | "Tokyo"     | 1000       | 100        | 1100        |
+      | "Beijing"   | 1000       | 100        | 1100        |
+
+  # Scenario: Determine the fees for a supported destination
+  #  Given the customer want to travel to "Paris"
+  #  And the travel fees are 800€
+  #  And the agency fees are 50€
+  #  When the system calculate the travel fees
+  #  Then the travel price is 850€
--- domain/src/test/java/com/heiwait/tripagency/domain/cucumber/steps/CalculateTravelFeesSteps.java
+++ domain/src/test/java/com/heiwait/tripagency/domain/cucumber/steps/CalculateTravelFeesSteps.java
@@ -1,6 +1,40 @@
 package com.heiwait.tripagency.domain.cucumber.steps;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculateTravelFeesSteps {
+    @Given("^the customer want to travel to \"([^\"]*)\"$")
+    public void the_customer_want_to_travel_to(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^the travel fees are (\\d+)€$")
+    public void the_travel_fees_are_€(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^the agency fees are (\\d+)€$")
+    public void the_agency_fees_are_€(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^the system calculate the trip price$")
+    public void the_system_calculate_the_trip_price() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the trip price is (\\d+)€$")
+    public void the_trip_price_is_€(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
 }
