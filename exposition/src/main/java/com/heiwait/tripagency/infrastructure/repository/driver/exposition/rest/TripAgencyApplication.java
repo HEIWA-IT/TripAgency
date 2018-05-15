2e91b144d8a1be97aef238adb25c6ad1c3de9d4e
Dan <dan@heiwa-it>
2018-05-15 14:27:34 +0200

    Configuration simplification

--- exposition/src/main/java/com/heiwait/tripagency/infrastructure/repository/driver/exposition/rest/TripAgencyApplication.java
+++ exposition/src/main/java/com/heiwait/tripagency/infrastructure/repository/driver/exposition/rest/TripAgencyApplication.java
@@ -1,12 +1,8 @@
 package com.heiwait.tripagency.infrastructure.repository.driver.exposition.rest;
 
-import com.heiwait.tripagency.domain.TravelPricer;
-import com.heiwait.tripagency.domain.TripRepositoryPort;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.heiwait.tripagency"})
