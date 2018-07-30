6879a79f97b343122b3bfffd632341534e7e7f63
Dan <dan@heiwa-it>
2018-07-30 23:49:59 +0200

    Using @GetMApping instead of @RequestMapping

--- exposition/src/main/java/com/heiwait/tripagency/infrastructure/repository/driver/exposition/rest/api/TripResource.java
+++ exposition/src/main/java/com/heiwait/tripagency/infrastructure/repository/driver/exposition/rest/api/TripResource.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/api")
@@ -22,7 +19,7 @@ public class TripResource {
     }
 
     @ApiOperation(value="Compute travel fees", notes="Returns the price of a trip")
-    @RequestMapping(value={"/trip/{destination}/calculateTripPrice"}, method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value={"/trip/{destination}/calculateTripPrice"}, produces=MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Integer> calculateTripPrice(
             @PathVariable(value="destination") String destinationName) {
         Destination destination = new Destination(destinationName);
