efda7819da6aec60330547236f38adb38864e6ad
Dan <dan@heiwa-it>
2018-08-01 16:23:26 +0200

    Improving the design by moving some code element from exposition to application

--- application/src/main/java/com/heiwait/tripagency/infrastructure/application/handler/PriceComputorHandler.java
+++ application/src/main/java/com/heiwait/tripagency/infrastructure/application/handler/PriceComputorHandler.java
@@ -1,5 +1,6 @@
 package com.heiwait.tripagency.infrastructure.application.handler;
 
+import com.heiwait.tripagency.domain.Destination;
 import com.heiwait.tripagency.domain.TravelPricer;
 import com.heiwait.tripagency.domain.TripRepositoryPort;
 import org.springframework.stereotype.Service;
@@ -10,13 +11,17 @@ public class PriceComputorHandler extends TravelPricer {
 
     private PriceComputorRepositoryManager priceComputorRepositoryManager;
 
-    public PriceComputorHandler(){}
-
     public PriceComputorHandler(final PriceComputorRepositoryManager priceComputorRepositoryManager){
         this.priceComputorRepositoryManager = priceComputorRepositoryManager;
     }
 
-    public void setTripRepository(TripRepositoryPort tripRepository) {
+    private void setTripRepository(TripRepositoryPort tripRepository) {
         this.tripRepository = tripRepository;
+    }
+
+    public Integer computeTravelPrice(final Destination destination, final RepositoryType repositoryType) {
+        TripRepositoryPort tripRepositoryAdapter = priceComputorRepositoryManager.getTripRepositoryAdapter(repositoryType);
+        this.setTripRepository(tripRepositoryAdapter);
+        return super.computeTravelPrice(destination);
     }
 }
--- exposition/src/main/java/com/heiwait/tripagency/infrastructure/repository/driver/exposition/rest/api/TripResource.java
+++ exposition/src/main/java/com/heiwait/tripagency/infrastructure/repository/driver/exposition/rest/api/TripResource.java
@@ -1,8 +1,6 @@
 package com.heiwait.tripagency.infrastructure.repository.driver.exposition.rest.api;
 
 import com.heiwait.tripagency.domain.Destination;
-import com.heiwait.tripagency.domain.PriceComputorDriverPort;
-import com.heiwait.tripagency.domain.TripRepositoryPort;
 import com.heiwait.tripagency.infrastructure.application.handler.PriceComputorHandler;
 import com.heiwait.tripagency.infrastructure.application.handler.PriceComputorRepositoryManager;
 import com.heiwait.tripagency.infrastructure.application.handler.RepositoryType;
@@ -17,12 +15,9 @@ import org.springframework.web.bind.annotation.*;
 public class TripResource {
 
     private final PriceComputorHandler priceComputorDriverPort;
-    private final PriceComputorRepositoryManager priceComputorRepositoryManager;
 
-    public TripResource(final PriceComputorHandler priceComputorDriverPort,
-                        final PriceComputorRepositoryManager priceComputorRepositoryManager) {
+    public TripResource(final PriceComputorHandler priceComputorDriverPort) {
         this.priceComputorDriverPort = priceComputorDriverPort;
-        this.priceComputorRepositoryManager = priceComputorRepositoryManager;
     }
 
     @ApiOperation(value="Compute travel fees", notes="Returns the price of a trip")
@@ -32,10 +27,8 @@ public class TripResource {
             @PathVariable(value="repositoryType") RepositoryType repositoryType) {
         Destination destination = new Destination(destinationName);
 
-        TripRepositoryPort tripRepositoryAdapter = priceComputorRepositoryManager.getTripRepositoryAdapter(repositoryType);
-        priceComputorDriverPort.setTripRepository(tripRepositoryAdapter);
+        Integer travelPrice = priceComputorDriverPort.computeTravelPrice(destination, repositoryType);
 
-        Integer travelPrice = priceComputorDriverPort.computeTravelPrice(destination);
         return new ResponseEntity<>(travelPrice, HttpStatus.OK);
     }
 }
