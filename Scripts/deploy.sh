b2e41047f7fb223795668fed5c7873e5c1af17f3
Dan <dan@heiwa-it.com>
2020-01-20 23:08:22 +0100

    Improving CD part

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "Deploying artifact to development";
-#ENV_FOLDER=$FOLDER_BASE_ENV-dev;
-#ssh pxl "cd \"$ENV_FOLDER\" && $CMD_DC_STOP && cd ./apps/artifacts/ && rm -f *.jar && $CMD_ARTIFACT_DOWNLOAD && $CMD_DC_UP";
+echo "Deploying artifact to $1";
+ssh pxl 'cd /home/upxl/containers/tripagency/$1 && pwd';
