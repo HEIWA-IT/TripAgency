2aba2a94d3356a0ea8583d52f2e5082a9e59f918
Dan <dan@heiwa-it.com>
2020-01-24 11:08:29 +0100

    Downloading the artifact in the CD part

--- CI_CD/build.sh
+++ CI_CD/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/.m2;
+echo "$SETTINGS_XML" >> ~/.m2/settings.xml;
+
 echo "Launching build project and code project analysis with sonar";
 mvn deploy sonar:sonar -Dsonar.host.url=$SONAR_URL -Dsonar.login=$SONAR_CREDENTIALS $MAVEN_SONAR_OPTIONS;
--- CI_CD/deploy.sh
+++ CI_CD/deploy.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
+########### Prérequis ###########
+apt-get update;
+apt-get install -y openssh-server;
+
+mkdir -p ~/.ssh;
+echo "$SSH_PRIVATE_KEY" >> ~/.ssh/docker-devtools.key;
+chmod 600 ~/.ssh/docker-devtools.key;
+echo "$SSH_CONFIG" >> ~/.ssh/config;
+
+ssh -o StrictHostKeyChecking=no pxl uptime;
+mkdir -p /opt/apps/conf;
+
+apt-get install -y jq;
+
+##################### Commands used #####################
+DOCKER_COMPOSE_STOP="docker-compose stop";
+CLEAN_ARTIFACT_FOLDER="cd ./apps/artifacts/ && rm -f *.jar";
+DOCKER_COMPOSE_UP="docker-compose up -d";
+#########################################################
+
+echo "downloadArtifact method";
+URL=https://nexus.paxleones.com;
+RESOURCES=service/rest/v1/search/assets;
+REPOSITORY=paxleones;
+GROUP=com.heiwait;
+NAME=exposition;
+BASE_VERSION=0.0.1-SNAPSHOT;
+EXTENSION=jar;
+
+ARTIFACT_URL=$(curl -u "$NEXUS_LOGIN":"$NEXUS_PASSWORD" -X GET "$URL/$RESOURCES?sort=version&direction=desc&repository=$REPOSITORY&group=$GROUP&name=$NAME&maven.baseVersion=$BASE_VERSION&maven.extension=$EXTENSION" -H  "accept: application/json" | jq '.items[0].downloadUrl');
+echo "url to dowload artifact: " $ARTIFACT_URL;
+#DOWNLOAD_ARTIFACT=$(curl -u dev:dev $GET_ARTIFACT_URL -fsSL -o exposition.jar);
+
+########## Strating deployment ##########################
 echo "Deploying artifact to $1";
-ssh pxl 'cd /home/upxl/containers/tripagency/'$1' && pwd';
+ENV_FOLDER=/home/upxl/containers/tripagency/$1;
+ssh pxl "cd $ENV_FOLDER && $DOCKER_COMPOSE_STOP && $CLEAN_ARTIFACT_FOLDER && curl -u dev:dev $ARTIFACT_URL -fsSL -o exposition.jar && $DOCKER_COMPOSE_UP";
