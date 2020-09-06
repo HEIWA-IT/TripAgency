af730f4749908b8974509a41003ca67583f1d056
Dan <dan@heiwa-it.com>
2020-09-06 10:17:20 +0200

    Fixing the issues to the TripRepositoryMockAdapter class reported by the sonarqube scan

--- domain/src/main/java/com/heiwait/tripagency/domain/Destination.java
+++ domain/src/main/java/com/heiwait/tripagency/domain/Destination.java
@@ -1,5 +1,7 @@
 package com.heiwait.tripagency.domain;
 
+import java.util.Objects;
+
 public class Destination {
     private String name;
 
@@ -15,5 +17,18 @@ public class Destination {
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Destination that = (Destination) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- infrastructure/src/main/java/com/heiwait/tripagency/infrastructure/repository/mock/TripRepositoryMockAdapter.java
+++ infrastructure/src/main/java/com/heiwait/tripagency/infrastructure/repository/mock/TripRepositoryMockAdapter.java
@@ -8,46 +8,34 @@ import com.heiwait.tripagency.domain.error.BusinessException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 @Repository
 @Qualifier("TripRepositoryMockAdapter")
 public class TripRepositoryMockAdapter implements TripRepositoryPort {
 
+    final Trip parisTrip = new Trip(new Destination("Paris"), 25, 250, 150);
+    final Trip lilleTrip = new Trip(new Destination("Lille"), 0, 0, 0);
+    final Trip newyorkTrip = new Trip(new Destination("New-York"), 100, 2000, 1000);
+    final Trip tokyoTrip = new Trip(new Destination("Tokyo"), 150, 3000, 1100);
+    final Trip beijingTrip = new Trip(new Destination("Beijing"), 120, 1200, 1200);
+
+    final Map<Destination, Trip> trips = Map.ofEntries(
+            Map.entry(new Destination("Paris"), parisTrip),
+            Map.entry(new Destination("Lille"), lilleTrip),
+            Map.entry(new Destination("New-York"), newyorkTrip),
+            Map.entry(new Destination("Tokyo"), tokyoTrip),
+            Map.entry(new Destination("Beijing"), beijingTrip)
+    );
+
     @Override
     public Trip findTripByDestination(final Destination destination) {
+        Trip findedTrip = trips.get(destination);
 
-        Trip trip = new Trip();
-        trip.setDestination(destination);
-
-        switch (destination.getName().toLowerCase()) {
-            case "paris":
-                trip.setStayFees(250);
-                trip.setAgencyFees(25);
-                trip.setTicketPrice(150);
-                break;
-            case "lille":
-                trip.setStayFees(0);
-                trip.setAgencyFees(0);
-                trip.setTicketPrice(0);
-                break;
-            case "new-york":
-                trip.setStayFees(2000);
-                trip.setAgencyFees(100);
-                trip.setTicketPrice(1000);
-                break;
-            case "tokyo":
-                trip.setStayFees(3000);
-                trip.setAgencyFees(150);
-                trip.setTicketPrice(1100);
-                break;
-            case "beijing":
-                trip.setStayFees(1200);
-                trip.setAgencyFees(120);
-                trip.setTicketPrice(1200);
-                break;
-            default:
-                throw new BusinessException(BusinessErrors.MISSING_DESTINATION);
+        if (findedTrip != null) {
+            return findedTrip;
+        } else {
+            throw new BusinessException(BusinessErrors.MISSING_DESTINATION);
         }
-
-        return trip;
     }
 }
