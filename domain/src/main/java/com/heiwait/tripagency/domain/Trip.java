ba904caad5e18139e9fa213df340c156076addf4
Dan <dan@heiwa-it.com>
2020-09-08 17:17:26 +0200

    Refactoring code to improve sonar metrics

--- domain/src/main/java/com/heiwait/tripagency/domain/Trip.java
+++ domain/src/main/java/com/heiwait/tripagency/domain/Trip.java
@@ -1,13 +1,11 @@
 package com.heiwait.tripagency.domain;
 
-import java.util.Objects;
-
 public class Trip {
 
-    private Destination destination;
-    private Integer agencyFees;
-    private Integer stayFees;
-    private Integer ticketPrice;
+    private final Destination destination;
+    private final Integer agencyFees;
+    private final Integer stayFees;
+    private final Integer ticketPrice;
 
     public static final Trip MISSING_DESTINATION = new Trip(new Destination("Missing destination"), 0, 0, 0);
 
@@ -16,26 +14,6 @@ public class Trip {
         this.agencyFees = agencyFees;
         this.stayFees = stayFees;
         this.ticketPrice = ticketPrice;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Trip trip = (Trip) o;
-        return Objects.equals(ticketPrice, trip.ticketPrice) &&
-                Objects.equals(destination, trip.destination) &&
-                Objects.equals(agencyFees, trip.agencyFees) &&
-                Objects.equals(stayFees, trip.stayFees);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(destination, agencyFees, stayFees, ticketPrice);
-    }
-
-    public Destination destination() {
-        return destination;
     }
 
     public Integer agencyFees() {
