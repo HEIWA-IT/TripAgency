9f8cbd3669960625def7a04fe3995561462a7d32
Dan <dan@heiwa-it.com>
2020-10-02 20:22:53 +0200

    [FE] Making the project compliant Java 8

--- infrastructure/src/main/java/com/heiwait/tripagency/infrastructure/repository/mock/TripRepositoryMockAdapter.java
+++ infrastructure/src/main/java/com/heiwait/tripagency/infrastructure/repository/mock/TripRepositoryMockAdapter.java
@@ -6,8 +6,8 @@ import com.heiwait.tripagency.domain.TripRepositoryPort;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @Repository
 @Qualifier("TripRepositoryMockAdapter")
@@ -19,18 +19,21 @@ public class TripRepositoryMockAdapter implements TripRepositoryPort {
     final Trip newyorkTrip = new Trip(150, 1500, 1000);
     final Trip tokyoTrip = new Trip(200, 2000, 1500);
 
-    final Map<String, Trip> trips = Map.ofEntries(
-            Map.entry("paris", parisTrip),
-            Map.entry("lille", lilleTrip),
-            Map.entry("new-york", newyorkTrip),
-            Map.entry("tokyo", tokyoTrip),
-            Map.entry("beijing", beijingTrip)
-    );
+    final Map<String, Trip> trips = new HashMap<String, Trip>() {{
+        put("paris", parisTrip);
+        put("lille", lilleTrip);
+        put("new-york", newyorkTrip);
+        put("tokyo", tokyoTrip);
+        put("beijing", beijingTrip);
+    }};
 
     @Override
     public Trip findTripByDestination(final Destination destination) {
         Trip findedTrip = trips.get(destination.name().toLowerCase());
 
-        return Objects.requireNonNullElse(findedTrip, Trip.MISSING_DESTINATION);
+        if (findedTrip != null)
+            return findedTrip;
+        else
+            return Trip.MISSING_DESTINATION;
     }
 }
