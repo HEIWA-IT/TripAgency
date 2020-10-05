a896b21f2ce83f30c2cdc505b39efa4d3747c053
Dan <dan@heiwa-it.com>
2020-10-05 14:07:48 +0200

    [FE] Making the project compliant Java 11 with Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,8 +70,8 @@ tasks.compileJava {
 subprojects {
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     val javaRestProjectVersion: String by project
