5f94f89593f6cba96362e3036705cbf894c49e3e
Dan <dan@heiwa-it.com>
2020-11-03 20:05:11 +0100

    [FE] Refactoring the check_pipeline_variables.sh script.

--- scripts/commons/check_pipeline_variables.sh
+++ scripts/commons/check_pipeline_variables.sh
@@ -34,25 +34,61 @@
 ################################################################################
 ################################################################################
 
-MANDATORY_VARIABLE=true
-NOT_MANDATORY_VARIABLE=false
+MANDATORY_VARIABLES=( "OPTIONS" "MAVEN_REPOSITORY" "SONARQUBE_URL" "SONARQUBE_CREDENTIALS" "DOCKER_PROJECT_REGISTRY"
+"DOCKER_REGISTRY_USERNAME" "DOCKER_REGISTRY_PASSWORD" "MAVEN_REPOSITORY" "CUKEDOCTOR_MAIN_JAR" )
 
-#######################################
-# Check if the variable as argument is filled
-# Arguments:
-#   - Variable name to check
-#   - Is the variable mandatory
-# Outputs:
-#   Writes if the variable is filled or not
+NON_MANDATORY_VARIABLES=( "MVN_SETTINGS" "E2E_TEST_MODE" )
+
+###################################################
+# Check if a variable is filled or not
 # Returns:
-#   0 if the mandatory variable is filled or if it is a not mandatory variable,  else 1.
-#######################################
+#   1if the variable is not filled
+####################################################
 function check_variable() {
-  var_name=$1
-  is_mandatory=$2
+    var_name=$1
+    eval var_value=\$"$var_name"
 
-  eval var_value=\$"$var_name"
-  ([ -z "${var_value}" ] && echo "${var_name}" is empty!!! && if ${is_mandatory}; then exit 1; fi || echo "${var_name}" filled)
+    if [ -z "${var_value}" ]
+    then
+      echo "${var_name}" is empty!!! && return 1
+    else
+        echo "${var_name}" filled
+    fi
+}
+
+
+#######################################
+# Check the mandatory variables for the makefile to execute the rules
+# Outputs:
+#   Writes the number of variables that are not filled
+# Returns:
+#   0 if all the mandatory variables are set,
+#   else the number of mandatory variables not set.
+#######################################
+function check_mandatory_variables() {
+  count=0
+  for var_name in "${MANDATORY_VARIABLES[@]}"; do
+    check_variable "$var_name"
+    count=$(($?+count))
+  done
+
+  echo There are "${count}" mandatory variable\(s\) not filled!
+  exit ${count}
+}
+
+#######################################
+# Check the non mandatory variables for the makefile to execute the rules
+# Outputs:
+#   Writes the number of variables that are not filled
+#######################################
+function check_non_mandatory_variables() {
+  count=0
+  for var_name in "${NON_MANDATORY_VARIABLES[@]}"; do
+    check_variable "$var_name"
+    count=$(($?+count))
+  done
+
+  echo There are "${count}" non mandatory variable\(s\) not filled!
 }
 
 #######################################
@@ -64,37 +100,8 @@ function check_variable() {
 #   else the number of mandatory variables not set.
 #######################################
 function check_variables() {
-  declare -i status=0
-
-  check_variable "OPTIONS" $MANDATORY_VARIABLE
-  status+=$?
-  check_variable "MVN_SETTINGS" $NOT_MANDATORY_VARIABLE
-  status+=$?
-  check_variable "MAVEN_REPOSITORY" $MANDATORY_VARIABLE
-  status+=$?
-
-  check_variable "SONARQUBE_URL" $MANDATORY_VARIABLE
-  status+=$?
-  check_variable "SONARQUBE_CREDENTIALS" $MANDATORY_VARIABLE
-  status+=$?
-
-  check_variable "DOCKER_PROJECT_REGISTRY" $MANDATORY_VARIABLE
-  status+=$?
-  check_variable "DOCKER_REGISTRY_USERNAME" $MANDATORY_VARIABLE
-  status+=$?
-  check_variable "DOCKER_REGISTRY_PASSWORD" $MANDATORY_VARIABLE
-  status+=$?
-
-  check_variable "MAVEN_REPOSITORY" $MANDATORY_VARIABLE
-  status+=$?
-  check_variable "CUKEDOCTOR_MAIN_JAR" $MANDATORY_VARIABLE
-  status+=$?
-
-  check_variable "E2E_TEST_MODE" $NOT_MANDATORY_VARIABLE
-  status+=$?
-
-  echo There are ${status} mandatory variable\(s\) not filled!
-  exit ${status}
+  check_non_mandatory_variables
+  check_mandatory_variables
 }
 
 ################################################################################
