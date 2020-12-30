c01e4d1d852df541fadba899d99221e1ad5792e8
Dan <dan@heiwa-it.com>
2020-12-30 13:58:24 +0100

    [FIX] Fixing the order of java keyword to match convention

--- domain/src/main/java/com/heiwait/tripagency/pricer/domain/error/ErrorMessagesProperties.java
+++ domain/src/main/java/com/heiwait/tripagency/pricer/domain/error/ErrorMessagesProperties.java
@@ -4,7 +4,7 @@ import java.util.ResourceBundle;
 
 public class ErrorMessagesProperties {
     private static final String FILENAME = "messages";
-    private final static ResourceBundle rb = ResourceBundle.getBundle(FILENAME);
+    private static final  ResourceBundle rb = ResourceBundle.getBundle(FILENAME);
 
     private ErrorMessagesProperties() {
     }
