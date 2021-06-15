78104d511c0ee79b61270ff7f487b7b1f25e482e
Dan <dan@heiwa-it.com>
2021-06-15 15:54:34 +0200

    [FIX] Renaming a variable and a test method to be more accurate

--- domain/src/test/java/com/heiwait/tripagency/pricer/domain/TripPricerTest.java
+++ domain/src/test/java/com/heiwait/tripagency/pricer/domain/TripPricerTest.java
@@ -1,6 +1,5 @@
 package com.heiwait.tripagency.pricer.domain;
 
-import com.heiwait.tripagency.pricer.domain.TripPricer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,11 +10,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @ExtendWith(MockitoExtension.class)
 class TripPricerTest {
     @InjectMocks
-    private TripPricer travelPricer;
+    private TripPricer tripPricer;
 
     @Test
-    void computeTravelPrice_should_return_an_invalid_argument_exception_if_destination_is_null() {
-        assertThatThrownBy(() -> travelPricer.priceTrip(null, null))
+    void should_return_an_invalid_argument_exception_if_destination_is_null() {
+        assertThatThrownBy(() -> tripPricer.priceTrip(null, null))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
