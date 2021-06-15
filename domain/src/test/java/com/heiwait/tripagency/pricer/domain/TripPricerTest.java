15af125e638de1ad2ac30bd7aae54d13c2675730
Dan <dan@heiwa-it.com>
2021-06-15 17:58:47 +0200

    [FE] Add @DisplayNameGeneration in TripPricerTest class to improve readability

--- domain/src/test/java/com/heiwait/tripagency/pricer/domain/TripPricerTest.java
+++ domain/src/test/java/com/heiwait/tripagency/pricer/domain/TripPricerTest.java
@@ -1,5 +1,7 @@
 package com.heiwait.tripagency.pricer.domain;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class TripPricerTest {
     @InjectMocks
     private TripPricer tripPricer;
